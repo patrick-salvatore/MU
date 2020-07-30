5a1acf9a635530ed11982f1d93af05b8f681d471
patrick-salvatore <salvatorepat8@gmail.com>
2020-07-30 16:59:07 -0400

    build fail

--- src/components/accordion/Summary.tsx
+++ src/components/accordion/Summary.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAccordionContext } from './AccordionContext';
 import ChevronRight from './chevron-right.svg';
-import ChevronDown from './chevron-Down.svg';
+import ChevronDown from './chevron-down.svg';
 
 import './summary.scss';
 
